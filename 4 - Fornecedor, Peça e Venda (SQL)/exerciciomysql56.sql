dec1033dc535ea9cedb4936a518fcccfac129604
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-07-02 10:56:25 -0300

    Add files via upload

---
+++ 4 - Fornecedor, Peça e Venda (SQL)/exerciciomysql56.sql
@@ -0,0 +1,36 @@
+-- MySQL 5.6
+-- 
+ 
+-- Comandos DDL
+CREATE TABLE Fornecedor (
+  CodForn INT AUTO_INCREMENT PRIMARY KEY,
+  Nome VARCHAR(100),
+  Cidade VARCHAR(80)
+);
+
+CREATE TABLE Peca (
+  CodPeca INT AUTO_INCREMENT PRIMARY KEY,
+  Nome VARCHAR(30),
+  Descricao VARCHAR(60)
+);
+
+CREATE TABLE Venda (
+  CodForn INT,
+  CodPeca INT,
+  Quantidade INT,
+  Data DATE,
+  FOREIGN KEY (CodForn) REFERENCES Fornecedor(CodForn),
+  FOREIGN KEY (CodPeca) REFERENCES Peca(CodPeca)
+);
+
+-- Comandos DML
+INSERT INTO Fornecedor (Nome, Cidade) VALUES ('Ajax','Limeira');
+INSERT INTO Peca (Nome, Descricao) VALUES ('Desengordurante','Limpa Gordura');
+INSERT INTO Venda (CodForn, CodPeca, Quantidade, Data)
+SELECT Fornecedor.CodForn, Peca.CodPeca, 5, '1997-07-02'
+FROM Fornecedor, Peca
+WHERE Fornecedor.Nome = 'Ajax' AND Peca.Nome = 'Desengordurante';
+
+SELECT * FROM Fornecedor;
+SELECT * FROM Peca;
+SELECT * FROM Venda;
